ba4d66d9dca796308f546bb37891d355d9df83df
zhangkx <kunloon@qq.com>
2019-07-08 23:13:07 +0800

    add default methods

--- src/main/java/com/marssvn/svnapi/ISvnAdmin.java
+++ src/main/java/com/marssvn/svnapi/ISvnAdmin.java
@@ -2,6 +2,8 @@ package com.marssvn.svnapi;
 
 import com.marssvn.svnapi.enums.ERepositoryType;
 
+import java.io.IOException;
+
 /**
  * svn admin interface
  *
@@ -16,8 +18,20 @@ public interface ISvnAdmin {
      * @param repoName repository name
      * @param repoType repository type, fsfs: FSFS(default), bdb: Berkeley DB
      * @return repository path
+     * @throws IOException IOException
      */
-    String createRepository(String rootPath, String repoName, ERepositoryType repoType);
+    String createRepository(String rootPath, String repoName, ERepositoryType repoType) throws IOException;
+
+    /**
+     * createRepository a new repository
+     *
+     * @param repoName repository name
+     * @return repository path
+     * @throws IOException IOException
+     */
+    default String createRepository(String repoName) throws IOException {
+        return createRepository(null, repoName, null);
+    }
 
     /**
      * moveRepository repository
@@ -25,13 +39,37 @@ public interface ISvnAdmin {
      * @param rootPath    root path
      * @param oldRepoName old repository name
      * @param newRepoName new repository name
+     * @throws IOException IOException
      */
-    void moveRepository(String rootPath, String oldRepoName, String newRepoName);
+    void moveRepository(String rootPath, String oldRepoName, String newRepoName) throws IOException;
+
+    /**
+     * moveRepository repository
+     *
+     * @param oldRepoName old repository name
+     * @param newRepoName new repository name
+     * @throws IOException IOException
+     */
+    default void moveRepository(String oldRepoName, String newRepoName) throws IOException {
+        moveRepository(null, oldRepoName, newRepoName);
+    }
 
     /**
      * deleteRepository repository
      *
-     * @param repoPath repository path
+     * @param rootPath root path
+     * @param repoName repository name
+     * @throws IOException IOException
      */
-    void deleteRepository(String repoPath);
+    void deleteRepository(String rootPath, String repoName) throws IOException;
+
+    /**
+     * deleteRepository repository
+     *
+     * @param repoName repository name
+     * @throws IOException IOException
+     */
+    default void deleteRepository(String repoName) throws IOException {
+        deleteRepository(null, repoName);
+    }
 }
