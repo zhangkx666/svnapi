6815cbf3ace2460191e9a630b91f02042ac1f696
zhangkx <kunloon@qq.com>
2019-07-15 23:34:33 +0800

    update test case

--- src/test/java/SVNClientImplTest.java
+++ src/test/java/SVNClientImplTest.java
@@ -2,10 +2,10 @@ import com.marssvn.svnapi.ISvnAdmin;
 import com.marssvn.svnapi.ISvnClient;
 import com.marssvn.svnapi.SvnAdminForLocale;
 import com.marssvn.svnapi.SvnClient;
+import com.marssvn.svnapi.exception.SvnApiException;
 import com.marssvn.svnapi.model.SvnUser;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.runners.MethodSorters;
 
 /**
  * SVNClientImpl Tester.
@@ -14,28 +14,29 @@ import org.junit.Test;
  * @version 1.0
  * @since <pre>2018-10-31</pre>
  */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SVNClientImplTest {
 
     private final static String REPOSITORY_NAME = "marssvn";
 
-    @Before
-    public void before() throws Exception {
+    @BeforeClass
+    public static void before() throws Exception {
         ISvnAdmin svnAdmin = new SvnAdminForLocale();
         svnAdmin.deleteRepository(REPOSITORY_NAME);
         svnAdmin.createRepository(REPOSITORY_NAME);
     }
 
-    @After
-    public void after() throws Exception {
+    @AfterClass
+    public static void after() throws Exception {
 //        ISvnAdmin svnAdmin = new SvnAdminForLocale();
 //        svnAdmin.deleteRepository(REPOSITORY_NAME);
     }
 
     /**
-     * Method: mkdir(String dirPath, String message)`
+     * mkdir, success
      */
     @Test
-    public void testMakeDir() {
+    public void test01_MakeDir() {
         SvnUser svnUser = new SvnUser("marssvn", "marssvn");
         ISvnClient svnClient = new SvnClient();
         svnClient.setRootPath("svn://localhost/" + REPOSITORY_NAME);
@@ -43,5 +44,26 @@ public class SVNClientImplTest {
         svnClient.mkdir("111/222/333", "test make dir 111/222/333");
         svnClient.mkdir("demo", "test make dir demo");
         svnClient.mkdir("src", "test make dir src");
+    }
+
+    /**
+     * mkdir, throw SvnApiException when SvnUser is null
+     */
+    @Test(expected = SvnApiException.class)
+    public void test02_MakeDir() {
+        ISvnClient svnClient = new SvnClient();
+        svnClient.setRootPath("svn://localhost/" + REPOSITORY_NAME);
+        svnClient.mkdir("demo2", "test make dir demo");
+    }
+
+    /**
+     * mkdir, throw SvnApiException when root path is null
+     */
+    @Test(expected = SvnApiException.class)
+    public void test03_MakeDir() {
+        SvnUser svnUser = new SvnUser("marssvn", "marssvn");
+        ISvnClient svnClient = new SvnClient();
+        svnClient.setSvnUser(svnUser);
+        svnClient.mkdir("demo3", "test make dir demo");
     }
 }
